f1da51c9a2a71fe5e153eee2d9ceb9a6ad63e13e
Shohidul Arifin <shohidularifin@gmail.com>
2023-09-25 22:34:50 -0700

    first commit

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "../pages/Shared/Navbar/NavBar";
+import Footer from "../pages/Shared/Footer/Footer";
+
+
+const Main = () => {
+    return (
+        <div>
+            <NavBar></NavBar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
